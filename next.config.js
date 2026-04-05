f80b72b509a8a0b22e63d3813ee7accb2a7a9f77
Solange <72896376+macintyresj@users.noreply.github.com>
2026-04-04 21:44:13 -0300

    Configure Next.js for standalone output and tracing

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'standalone',
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': [
+        'node_modules/@swc/core-linux-x64-gnu',
+        'node_modules/@swc/core-linux-x64-musl',
+      ],
+    },
+  },
+}
 module.exports = nextConfig
